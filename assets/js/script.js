eb2dd9851dc1c6e5201acd0ebb2fdb064ffaa3ba
afi <ahmadfaizal.i.afi@gmail.com>
2021-12-14 15:17:32 +0700

    Kontrakkan

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,15 @@
 const year = document.getElementById('year');
 year.innerHTML = new Date().getFullYear();
+
+$(document).ready(function(){
+    
+    $("#navigation li a, #header small a").click(function(e){
+        e.preventDefault();
+
+        var targetElement = $(this).attr("href");
+        var targetPosition = $(targetElement).offset().top;
+        
+        $("html, body").animate({ scrollTop: targetPosition - 100}, "slow")
+    });
+
+});
